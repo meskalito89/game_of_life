79222d5ed627fa900ba0e62129723ffa204534eb
Egor Rokin <rokinea89mone@mail.ru>
2019-02-05 12:50:25 +0500

    Updates

---
+++ precache-manifest.3cbebc471048e1b39e1971a498f3f758.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "62d4d0d7dc91ad4d6bcb",
+    "url": "/game_of_life/static/js/runtime~main.62d4d0d7.js"
+  },
+  {
+    "revision": "c88bde9a81229e4a8cef",
+    "url": "/game_of_life/static/js/main.c88bde9a.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/game_of_life/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "a4ee859b887ff2c8fce229a08b2cd3ce",
+    "url": "/game_of_life/index.html"
+  }
+];
